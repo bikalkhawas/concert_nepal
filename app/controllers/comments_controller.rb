6ffd7093589c7fcb3edaf499830507ef1423997e
bikalkhawas <dhami.khawas@gmail.com>
2016-08-29 18:09:14 +0545

    user modified, only signed_in user can comment for the post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,25 +1,30 @@
 class CommentsController < ApplicationController
-	before_action :correct_user, only: :destroy
+  before_action :correct_user, only: :destroy
+    
+    def create
+       @post = Post.find(params[:post_id])      
+	       unless user_signed_in?
+	         flash[:notice] = "you must log_in to comment for the post"
+	         redirect_to new_user_session_path
+	      	 else
+		       	@comment = @post.comments.create(comment_params)
+		        @comment.user_id = current_user.id
 
-	def create
-		@post = Post.find(params[:post_id])
-		@comment = @post.comments.create(params[:comment].permit(:name, :body))
-        @comment.user_id = current_user.id
-        @comment.save
-
-
-		redirect_to post_path(@post)		
-	end
-
-	def destroy
-		@post = Post.find(params[:post_id])
-		@comment = @post.comments.find(params[:id])
-		@comment.destroy
-
-		redirect_to post_path(@post)
-	end
-
-	private
+		        @comment.save
+		        redirect_to post_path(@post)
+      	    end
+    end
+        
+    
+    def destroy
+      @post = Post.find(params[:post_id])
+      @comment = @post.comments.find(params[:id])
+      @comment.destroy
+      
+      redirect_to post_path(@post)
+    end
+    
+    private
       
     def comment_params
       params.require(:comment).permit(:name, :body)
@@ -32,5 +37,6 @@ class CommentsController < ApplicationController
         redirect_to :back
       end
     end
+    
 
 end
